774c68564eea5574eb965c880fe4319c55959276
JosephQQQ <elmoukhtar.mokhtari@my.jcu.edu.au>
2018-12-09 17:34:21 +0800

    test numero trois

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# HelloWorld3
+# angular2.0
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.1.2.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to hello-world3!');
+    expect(page.getTitleText()).toEqual('Welcome to angular2.0!');
   });
 });
